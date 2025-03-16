8408fcb3fa35d901754e2133f8dad804e9d27979
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-03-16 21:04:26 +0200

    fix

--- src/components/BitcoinTrafic/BitcoinTrafic.tsx
+++ src/components/BitcoinTrafic/BitcoinTrafic.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { Transaction } from "../types/Transaction.type";
 import { Table } from "./Table";
