2648f5948bec13c6c09e0fd8d886597011c4ef8a
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-03-16 21:03:19 +0200

    add hook

--- src/components/BitcoinTrafic/BitcoinTrafic.tsx
+++ src/components/BitcoinTrafic/BitcoinTrafic.tsx
@@ -1,56 +1,16 @@
-import { useState, useEffect, useRef } from "react";
-import { BITCOIN_WS_URL } from "../../constants/constants";
+import { useState, useEffect } from "react";
 import { Transaction } from "../types/Transaction.type";
 import { Table } from "./Table";
+import { useWsConnect } from "../../hooks/useWsConnect";
 import styles from "./BitcoinTrafic.module.scss";
-
-const connection = JSON.stringify({ op: "unconfirmed_sub" });
 
 export const BitcoinTrafic = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [totalBTC, setTotalBTC] = useState(0);
-  const ws = useRef<WebSocket | null>(null);
   const [isStart, setIsStart] = useState(false);
 
-  const handleMessage = (event: MessageEvent) => {
-    const data = JSON.parse(event.data);
-
-    if (data.op === "utx") {
-      const newTransaction: Transaction = data.x;
-
-      setTransactions((prev) => {
-        const updatedTransactions = [newTransaction, ...prev];
-        return updatedTransactions.slice(0, 5);
-      });
-
-      const total = newTransaction.out.reduce((sum, out) => sum + out.value, 0);
-      setTotalBTC((prev) => prev + total / 100000000);
-    }
-  };
-
-  const connectWebSocket = () => {
-    ws.current = new WebSocket(BITCOIN_WS_URL);
-    ws.current.onopen = () => {
-      ws.current?.send(connection);
-    };
-    ws.current.onmessage = handleMessage;
-    ws.current.onerror = (error) => console.error("WebSocket Error:", error);
-    ws.current.onclose = () => console.log("WebSocket Disconnected");
-    setIsStart(true);
-  };
-
-  const disconnectWebSocket = () => {
-    ws.current?.close();
-    ws.current = null;
-    setIsStart(false);
-  };
-
-  const resetTransactions = () => {
-    setTransactions([]);
-    setTotalBTC(0);
-    disconnectWebSocket();
-    setIsStart(false);
-  };
+  const { connectWebSocket, disconnectWebSocket, resetTransactions } =
+    useWsConnect({ setTransactions, setTotalBTC, setIsStart });
 
   useEffect(() => {
     return () => disconnectWebSocket();
@@ -59,18 +19,11 @@ export const BitcoinTrafic = () => {
   return (
     <div className={styles.wrapper}>
       <h2>Bitcoin Transaction Tracker</h2>
-      <button
-        className={styles.button}
-        onClick={connectWebSocket}
-        disabled={isStart}>
+      <button onClick={connectWebSocket} disabled={isStart}>
         Start
       </button>
-      <button className={styles.button} onClick={disconnectWebSocket}>
-        Stop
-      </button>
-      <button className={styles.button} onClick={resetTransactions}>
-        Reset
-      </button>
+      <button onClick={disconnectWebSocket}>Stop</button>
+      <button onClick={resetTransactions}>Reset</button>
 
       <p>Sum = {totalBTC.toFixed(8)} BTC</p>
 
---
+++ src/hooks/useWsConnect.ts
@@ -0,0 +1,61 @@
+import { useRef } from "react";
+import { Transaction } from "../components/types/Transaction.type";
+import { BITCOIN_WS_URL } from "../constants/constants";
+
+const connection = JSON.stringify({ op: "unconfirmed_sub" });
+
+type Arg = {
+  setTransactions: React.Dispatch<React.SetStateAction<Transaction[]>>;
+  setTotalBTC: React.Dispatch<React.SetStateAction<number>>;
+  setIsStart: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const useWsConnect = ({
+  setTransactions,
+  setTotalBTC,
+  setIsStart,
+}: Arg) => {
+  const ws = useRef<WebSocket | null>(null);
+
+  const handleMessage = (event: MessageEvent) => {
+    const data = JSON.parse(event.data);
+
+    if (data.op === "utx") {
+      const newTransaction: Transaction = data.x;
+
+      setTransactions((prev) => {
+        const updatedTransactions = [newTransaction, ...prev];
+        return updatedTransactions.slice(0, 5);
+      });
+
+      const total = newTransaction.out.reduce((sum, out) => sum + out.value, 0);
+      setTotalBTC((prev) => prev + total / 100000000);
+    }
+  };
+
+  const connectWebSocket = () => {
+    ws.current = new WebSocket(BITCOIN_WS_URL);
+    ws.current.onopen = () => {
+      ws.current?.send(connection);
+    };
+    ws.current.onmessage = handleMessage;
+    ws.current.onerror = (error) => console.error("WebSocket Error:", error);
+    ws.current.onclose = () => console.log("WebSocket Disconnected");
+    setIsStart(true);
+  };
+
+  const disconnectWebSocket = () => {
+    ws.current?.close();
+    ws.current = null;
+    setIsStart(false);
+  };
+
+  const resetTransactions = () => {
+    setTransactions([]);
+    setTotalBTC(0);
+    disconnectWebSocket();
+    setIsStart(false);
+  };
+
+  return { connectWebSocket, disconnectWebSocket, resetTransactions };
+};
