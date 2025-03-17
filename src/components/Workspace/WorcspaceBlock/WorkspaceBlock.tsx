d884ec998880af739a4a1bd95aa0ebdb6c0221ab
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-03-17 11:22:21 +0200

    refactoring

--- src/components/Workspace/WorcspaceBlock/WorkspaceBlock.tsx
+++ src/components/Workspace/WorcspaceBlock/WorkspaceBlock.tsx
@@ -4,7 +4,6 @@ import Draggable from "react-draggable";
 import styles from "../Workspace.module.scss";
 import { Block } from "../../../types/Block.type";
 import { BlockContent } from "./BlockContent";
-import { useDragResize } from "../../../hooks/useDragResize";
 
 type Props = {
   block: Block;
@@ -15,10 +14,24 @@ export const WorkspaceBlock: React.FC<Props> = ({ block, setBlocks }) => {
   const nodeRef = useRef<HTMLDivElement>(null) as RefObject<HTMLDivElement>;
   const [isResizing, setIsResizing] = useState(false);
 
-  const { resizeBlock, dragBlock, removeBlock } = useDragResize({
-    setBlocks,
-    setIsResizing,
-  });
+  const resizeBlock = (blockId: number, width: number, height: number) => {
+    setBlocks((prev) =>
+      prev.map((block) =>
+        block.id === blockId ? { ...block, width, height } : block
+      )
+    );
+    setIsResizing(false);
+  };
+
+  const dragBlock = (blockId: number, x: number, y: number) => {
+    setBlocks((prev) =>
+      prev.map((block) => (block.id === blockId ? { ...block, x, y } : block))
+    );
+  };
+
+  const removeBlock = (blockId: number) => {
+    setBlocks((prev) => prev.filter((item) => item.id !== blockId));
+  };
 
   return (
     <div className={styles.wrapper}>
